408d19dbab84b75f2c6ca3b2f9f9bba017dd13a6
yahooserg <seltexinc@gmail.com>
2017-07-27 18:21:21 +0300

    add activatedrouteoptions with parametered link using ngClass and function

--- src/app/corp/main/trade/trade.component.ts
+++ src/app/corp/main/trade/trade.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Router,
+  ActivatedRoute
+} from '@angular/router';
 
 @Component({
   selector: 'app-trade',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TradeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+  }
+
+  homeIsActive() {
+    if (('/' + this.route.snapshot.params.company) === this.router.url) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
