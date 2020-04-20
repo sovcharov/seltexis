71a87c2674d52c4a2a3c0bbcc968d167456af494
yahooserg <seltexinc@gmail.com>
2020-04-20 17:37:30 +0700

    permalink done

--- src/app/corp/main/trade/it/permalink/permalink.component.ts
+++ src/app/corp/main/trade/it/permalink/permalink.component.ts
@@ -11,6 +11,7 @@ export class PermalinkComponent implements OnInit {
 
   loading: boolean;
   inventoryToSearch: string = "";
+  checkAll: boolean = false;
   constructor(
     public inventoryService: InventoryService
   ) { }
@@ -22,8 +23,51 @@ export class PermalinkComponent implements OnInit {
     this.loading = true;
     this.inventoryService.inventoryPermalinks = [];
     this.inventoryService.getInventoryForPermalinks(() => {
+      for(let i = 0; i < this.inventoryService.inventoryPermalinks.length; i += 1) {
+        this.inventoryService.inventoryPermalinks[i].numbersString ="";
+        this.inventoryService.inventoryPermalinks[i].checked = false;
+        this.inventoryService.inventoryPermalinks[i].permalink = {text:"", loading: false};
+
+
+        for (let j = 0; j < this.inventoryService.inventoryPermalinks[i].numbers.length; j += 1) {
+            this.inventoryService.inventoryPermalinks[i].numbersString += `${this.inventoryService.inventoryPermalinks[i].numbers[j].number}-${this.inventoryService.inventoryPermalinks[i].numbers[j].manufacturerFullName} `;
+
+        }
+      }
+      // console.log(this.inventoryService.inventoryPermalinks);
       this.loading = false;
     });
+  }
+
+  public generatePermaLinks() {
+    for(let i = 0; i < this.inventoryService.inventoryPermalinks.length; i += 1) {
+      if (this.inventoryService.inventoryPermalinks[i].checked) {
+        this.inventoryService.inventoryPermalinks[i].permalink.loading = true;
+        this.inventoryService.getRecommendedUrlForItem(this.inventoryService.inventoryPermalinks[i], (res) => {
+          this.inventoryService.inventoryPermalinks[i].permalink.text = res.text;
+          this.inventoryService.inventoryPermalinks[i].permalink.loading = false;
+
+        });
+      }
+    }
+  }
+
+  public savePermaLinks(i) {
+    for(let i = 0; i < this.inventoryService.inventoryPermalinks.length; i += 1) {
+      if (this.inventoryService.inventoryPermalinks[i].checked) {
+        this.inventoryService.inventoryPermalinks[i].permalink.loading = true;
+        this.inventoryService.updateInventoryUrl(this.inventoryService.inventoryPermalinks[i].id, this.inventoryService.inventoryPermalinks[i].permalink.text, (res) => {
+          this.inventoryService.inventoryPermalinks[i].permalink.loading = false;
+
+        });
+      }
+    }
+  }
+
+  checkAllItems () {
+    for(let i = 0; i < this.inventoryService.inventoryPermalinks.length; i += 1) {
+      this.inventoryService.inventoryPermalinks[i].checked = this.checkAll;
+    }
   }
 
 }
